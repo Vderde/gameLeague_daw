8dca4b9b4711933b506ad9020eced9859635f2fd
Vderde <107716718+Vderde@users.noreply.github.com>
2022-11-16 10:19:15 +0100

    Add files via upload

---
+++ view/modules/endView.php
@@ -0,0 +1,8 @@
+
+<script src="view/js/jquery-3.3.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="view/js/interfaceWeb.js"></script>
+
+</body>
+</html>
